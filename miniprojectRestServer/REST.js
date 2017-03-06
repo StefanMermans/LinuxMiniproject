66db325c00d27e6c7ab6d913a8d0e75c4795b73f
Stefan Mermas <stefan_mermans@hotmail.com>
2017-03-06 14:42:03 +0100

    some js

--- miniprojectRestServer/REST.js
+++ miniprojectRestServer/REST.js
@@ -1,15 +1,77 @@
+var mysql = require("mysql");
+
 function REST_ROUTER(router,connection,md5) {
     var self = this;
     self.handleRoutes(router,connection,md5);
 }
 
+// Send a query to the database
+function SendQuery(connection,query){
+    connection.query(query, function(err,rows){
+        if(err){
+            res.json({"Status" : "Error"});
+            throw err;
+        } else {
+            res.json({"Status" : "Succes", "Data" : rows});
+        }
+    });
+}
+
 REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
     router.get("/",function(req,res){
-        res.json({"Message" : "Hello World !"});
-    })
-    router.get("/test", function(req, res){
-	res.json({"Message" : "No hello world!"});
-    })
+        res.json({"Message" : "Hello, welcome to our domotica API"});
+    });
+
+    router.get("/sensortype/:type", function(req, res){
+        // Create a SQL query using mysql.escape for safety reasons.
+        var query = `
+            SELECT * FROM COMBINED_MEASUREMENTS
+            WHERE SENSOR_TYPE_NAME = `+
+            mysql.escape(req.params.type);
+
+            connection.query(query, function(err,rows){
+                if(err){
+                    res.json({"Status" : "Error"});
+                    throw err;
+                } else {
+                    res.json({"Status" : "Succes", "Data" : rows});
+                }
+            });
+    });
+
+    router.get("/sensor/:id", function(req, res){
+        // Create a SQL query using mysql.escape for safety reasons.
+        var query = `
+            SELECT * FROM SENSOR_MEASUREMENTS
+            WHERE SENSOR_ID = `+
+            mysql.escape(req.params.id);
+
+            connection.query(query, function(err,rows){
+                if(err){
+                    res.json({"Status" : "Error"});
+                    throw err;
+                } else {
+                    res.json({"Status" : "Succes", "Data" : rows});
+                }
+            });
+    });
+
+    router.get("/device/:id", function(req, res){
+        // Create a SQL query using mysql.escape for safety reasons.
+        var query = `
+            SELECT * FROM DEVICE_SENSORS
+            WHERE DEVICE_ID = `+
+            mysql.escape(req.params.id);
+
+            connection.query(query, function(err,rows){
+                if(err){
+                    res.json({"Status" : "Error"});
+                    throw err;
+                } else {
+                    res.json({"Status" : "Succes", "Data" : rows});
+                }
+            });
+    });
 }
 
 module.exports = REST_ROUTER;
