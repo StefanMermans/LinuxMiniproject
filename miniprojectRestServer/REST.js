c7b4fe0ed52903b01aa60376bc4054aeaa3682dd
Bart Machielsen <bartmachielsen@gmail.com>
2017-03-23 08:57:39 +0100

    Added REST changes for registering

--- miniprojectRestServer/REST.js
+++ miniprojectRestServer/REST.js
@@ -56,93 +56,100 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
           return;
       }
 
-      if(req.body.id == null || req.body.ip == null){
+      if(req.body.ip == null){
         res.json({"Status" : "Error", "error":"missing params"});
         console.log("missing params! " + req.body);
         return;
       }
 
-        var id = req.body.id;
         var ip = req.body.ip;
-          var query = `
-            SELECT * FROM DEVICE WHERE IDENTIFIER = `+
-            mysql.escape(id);
-          console.log(query);
-          connection.query(query, function(err,rows){
-              if(err){
-                  res.json({"Status" : "Error"});
-                  console.log("SQL Error " + err);
-                  return;
-              } else {
-                  if(rows.length == 0){
-                    query = `
-                      INSERT INTO DEVICE (IP,IDENTIFIER) VALUES("`+
-                      mysql.escape(ip)+`",`+
-                      mysql.escape(id) + `)`;
-                      connection.query(query, function(err,rows){
-                          if(err){
-                              res.json({"Status" : "Error"});
-                              console.log("SQL Error " + err);
-                              return;
-                          }else{
-                            console.log("registered new");
-                          }
-                      });
+        var device_id = null;
+        var sensors = req.body.sensors;
+        connection.query("SELECT * FROM SENSOR", function(err,rows){
+            if(err){
+                res.json({"Status" : "Error"});
+                console.log("SQL Error " + err);
+                return;
+            } else {
+                var found_sensors = [];
+                console.log(rows);
+                for (var row in rows) {
+                  var found = null;
+                  var db_sensor = rows[row];
+                  for(var sensor in sensors){
+                    console.log("SENSOR RECEIVED:" + sensor);
+                    console.log(db_sensor);
+                    if(sensor == db_sensor["IDENTIFIER"]){
+                      found = db_sensor;
+                      device_id = db_sensor["DEVICE_ID"];
+                      break;
+                    }
                   }
-                    // TODO UPDATE IP IN SERVER!
-                    // TODO SHOW THAT DEVICE IS UPDATED IN SERVER (LAST_UPDATE IN DEVICE?)
-                    var device_id = rows[0].ID;
-                    query = `
-                      SELECT * FROM DEVICE_SENSORS WHERE DEVICE_ID = `+
-                      mysql.escape(device_id)
-                      console.log(query);
-                      connection.query(query, function(err,rows){
-                          if(err){
-                              res.json({"Status" : "Error"});
-                              console.log("SQL Error " + err);
-                              return;
-                          } else {
-                              res.json(
-                                {
-                                  "Status" : "Registered",
-                                  "ip":ip,
-                                  "identifier": id,
-                                  "device_id" : device_id,
-                                  "sensors":rows
-                                });
-                          }
-                      });
-              }
-          });
+                  if(found != null){
+                    console.log(found);
+                    console.log("FOUND SENSOR");
+                    found_sensors.push({"id":found["IDENTIFIER"],"database_id":found["ID"]});
+                  }
+                }
+                console.log(found_sensors);
+                if(device_id == null){
+                  console.log("INVALID DEVICE ID!");
+                  res.json(
+                    {
+                      "Status" : "Error",
+                      "error":"Invalid device id"
+                    });
+                }else{
+                  console.log("DEVICE REGISTERED ["+ device_id +"]");
+                  res.json(
+                    {
+                      "Status" : "Registered",
+                      "ip":ip,
+                      "device":device_id,
+                      "found":found_sensors
+                    });
+                }
+            }
+
+        });
     });
 
 
 
     // Method for posting a new measurement to the database
     router.post("/sensor/:id", function(req, res){
+        console.log("REQUEST!");
         try {
             var json_value = req.body;
             var params = req.params;
+            var value = req.body.value;
+            var id = req.params.id;
+            console.log(json_value);
+            console.log(params);
         } catch (e) {
             res.json({"Status" : "Error", "error":"invalid body/params"});
             return;
         }
 
-        if(req.body.value == null || req.params.id == null){
+        if(value == null || id == null){
           res.json({"Status" : "Error", "error":"missing params"});
           console.log("missing params! " + req.body);
           return;
         }
+        console.log("got a question from stefan..");
+        //console.log(req);
         var query = `
             INSERT INTO MEASUREMENT (SENSOR_ID, VALUE) VALUES(`+
-            mysql.escape(req.params.id) + `,`
-            +mysql.escape(req.body.value) + `);`;
+            mysql.escape(id) + `,`
+            +mysql.escape(value) + `);`;
+        console.log(query)
         connection.query(query, function(err,rows){
             if(err){
                 res.json({"Status" : "Error", "error":"Database Fail!"});
                 console.log("SQL Error " + err);
                 return;
             } else {
+                console.log("SUCCES!!")
                 res.json({"Status" : "Succes"});
             }
         });
